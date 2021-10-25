2b09b53514558ae779ff8c8c7351861266ab2684
Federica Milinanni <federica.milinanni@studenti.polito.it>
2021-10-25 16:04:57 +0200

    change variable name
    change variable name

--- import_from_SBtab.R
+++ import_from_SBtab.R
@@ -1,14 +1,14 @@
 remotes::install_github("a-kramer/SBtabVFGEN")
 
-import_from_SBtab <- function(tsvDirectory){
+import_from_SBtab <- function(SBtabDir){
 
-	setwd(tsvDirectory)
+	setwd(SBtabDir)
 	tsvList = dir(pattern = ".*[.]tsv$")
 	sbtab_model = SBtabVFGEN::sbtab_from_tsv(tsvList)
 	SBtabVFGEN::sbtab_to_vfgen(sbtab_model)		#this function created a .vf file (located in the tsvDirectory) from the SBtab model
 
 	vfFileName = dir(pattern = ".*[.]vf$")
-	system(paste("cd", tsvDirectory))
+	system(paste("cd", SBtabDir))
 	system(paste("vfgen r", vfFileName))
 	
 	return(sbtab_model)
