909d8b3b1f7a850821ee21f012140ae8de6e7eae
Federica Milinanni <federica.milinanni@studenti.polito.it>
2021-10-28 19:34:27 +0200

    .

--- import_from_SBtab.R
+++ import_from_SBtab.R
@@ -1,11 +1,10 @@
 remotes::install_github("a-kramer/SBtabVFGEN")
 
 import_from_SBtab <- function(SBtabDir){
-
 	setwd(SBtabDir)
 	tsvList = dir(pattern = ".*[.]tsv$")
 	sbtab_model = SBtabVFGEN::sbtab_from_tsv(tsvList)
-	SBtabVFGEN::sbtab_to_vfgen(sbtab_model)		#this function created a .vf file (located in the tsvDirectory) from the SBtab model
+	SBtabVFGEN::sbtab_to_vfgen(sbtab_model, cla = FALSE)		#this function created a .vf file (located in the tsvDirectory) from the SBtab model
 
 	vfFileName = dir(pattern = ".*[.]vf$")
 	system(paste("cd", SBtabDir))
