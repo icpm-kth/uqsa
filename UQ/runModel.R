fdcedbf8eaae6c5335c7ae743146292eec7e9fe2
Federica Milinanni <federica.milinanni@studenti.polito.it>
2022-01-28 09:27:47 +0100

    New runModel func with parallelization and C interface

--- UQ/runModel.R
+++ UQ/runModel.R
@@ -1,10 +1,9 @@
-remotes::install_github("a-kramer/rgsl", ref="OpenMP")
+remotes::install_github("a-kramer/rgsl") #, ref="OpenMP")
 
-runModel <- function(y0, modelFunctionName, params, inputs, outputTimes, outputFunction, environment="R", mc.cores = 8){
-
+runModel <- function(y0, modelFunctionName, params_inputs, outputTimes, outputFunction, environment="R", mc.cores = 8){
+    
   if(environment=="C")
   {
-    
     LIBS <- "-lgsl -lgslcblas -lm"
     CFLAGS <- "-shared -fPIC -Wall -O2"
     so <- sprintf("%s.so",modelFunctionName)
@@ -12,32 +11,30 @@ runModel <- function(y0, modelFunctionName, params, inputs, outputTimes, outputF
       system2("gcc",sprintf("%s -o %s %s_gvf.c %s",CFLAGS,so,modelFunctionName,LIBS))
     }
     
-    n_inputs <- length(inputs)
-    param_mat <- matrix(params,nrow=length(params),ncol=n_inputs)
-    input_mat <- do.call(cbind,inputs)
-    p <- rbind(param_mat,input_mat)
-    
     if (require("rgsl") && require("parallel")){
-      yy_gsl<-r_gsl_odeiv2(modelFunctionName,outputTimes,y0,p)
+      N <- ncol(params_inputs)
+      y0 <- matrix(rep(y0,N),ncol=N)
+      yy_gsl<-r_gsl_odeiv2(modelFunctionName,outputTimes,y0,params_inputs)
       yy_gsl_as_list <- mclapply(seq(dim(yy_gsl)[3]), function(x) yy_gsl[ , , x], mc.preschedule = FALSE, mc.cores = mc.cores)
-      output_yy <- mclapply(yy_gsl_as_list, outputFunction, mc.preschedule = FALSE, mc.cores = mc.cores)
+      output_yy <- mclapply(yy_gsl_as_list, function(yy_) apply(yy_,2,outputFunction), mc.preschedule = FALSE, mc.cores = mc.cores)
     }
   }
   else
   {
     if(environment!="R")
     {
-      str <- sprintf("%s is not admitted as value for the variable 'environment'. The default value 'R' will be used instead.",environment)
+      str <- sprintf("%s is not admitted as value for the variable 'environment'. The default value 'R' will be used instead.", environment)
       warning(str)
     }
     source(sprintf("%s.R",modelFunctionName))
     func <- eval(as.name(modelFunctionName))
     if(require("deSolve") && require("parallel")){
-      yy <- mclapply(inputs, function(inp) matrix(t(lsode(y0, outputTimes, func=func, parms=c(params,inp))[, -1]),ncol=length(outputTimes)), mc.preschedule = FALSE, mc.cores = mc.cores)
-      output_yy <- mclapply(yy, outputFunction, mc.preschedule = FALSE, mc.cores = mc.cores)
+      
+      params_inputs_as_list <- mclapply(seq(dim(params_inputs)[2]), function(x) params_inputs[,x], mc.preschedule = FALSE, mc.cores = mc.cores)
+      yy <- mclapply(params_inputs_as_list, function(p) matrix(t(lsode(y0, outputTimes, func=func, parms=p)[, -1]),ncol=length(outputTimes)), mc.preschedule = FALSE, mc.cores = mc.cores)
+      output_yy <- mclapply(yy, function(yy_) apply(yy_,2,outputFunction), mc.preschedule = FALSE, mc.cores = mc.cores)
     }
   }
-  #return(matrix(unlist(yy),ncol=length(yy)))
   return(output_yy)
 }
 
--- import_from_SBtab.R
+++ import_from_SBtab.R
@@ -1,16 +1,17 @@
 remotes::install_github("a-kramer/SBtabVFGEN")
 
-import_from_SBtab <- function(SBtabDir){
-	setwd(SBtabDir)
-	tsvList = dir(pattern = ".*[.]tsv$")
-	sbtab_model = SBtabVFGEN::sbtab_from_tsv(tsvList)
-	SBtabVFGEN::sbtab_to_vfgen(sbtab_model, cla = FALSE)		#this function created a .vf file (located in the tsvDirectory) from the SBtab model
-
-	vfFileName = dir(pattern = ".*[.]vf$")
-	system(paste("cd", SBtabDir))
-	system(paste("vfgen r", vfFileName))
-	system(paste("vfgen gsl", vfFileName))
-	
-	return(sbtab_model)
+import_from_SBtab <- function(SBtabDir, generateVFGEN = TRUE){
+  setwd(SBtabDir)
+  tsvList = dir(pattern = ".*[.]tsv$")
+  sbtab_model = SBtabVFGEN::sbtab_from_tsv(tsvList)
+  
+  if(generateVFGEN){
+    SBtabVFGEN::sbtab_to_vfgen(sbtab_model, cla = FALSE)		#this function created a .vf file (located in the tsvDirectory) from the SBtab model
+    vfFileName = dir(pattern = ".*[.]vf$")
+    system(paste("cd", SBtabDir))
+    system(paste("vfgen r:func=yes", vfFileName))
+    system(paste("vfgen gsl", vfFileName))
+  }
+  
+  return(sbtab_model)
 }
-
