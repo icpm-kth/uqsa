a667e21cf418f29c38aae176e09dcf03ca4a499a
Federica Milinanni <federica.milinanni@studenti.polito.it>
2022-03-09 14:06:08 +0100

    Create R package

---
+++ UQ/R/runModel.R
@@ -0,0 +1,35 @@
+runModel <- function(y0, modelName, params_inputs, outputTimes_list, outputFunctions_list, environment="R", mc.cores = 8){
+  
+  if(environment=="C")
+  {
+    LIBS <- "-lgsl -lgslcblas -lm"
+    CFLAGS <- "-shared -fPIC -Wall -O2"
+    so <- sprintf("%s.so",modelName)
+    if (!file.exists(so)){
+      system2("gcc",sprintf("%s -o %s %s_gvf.c %s",CFLAGS,so,modelName,LIBS))
+    }
+    
+    N <- dim(params_inputs)[2]
+    outputTimes <- outputTimes_list[[1]] ##TO CHANGE WHEN WE WILL HAVE A MORE GENERAL r_gsl_odeiv2 THAT ACCEPTS DIFFERENT OUTPUTTIMES FOR EACH PARAMETER/INPUT SET
+    yy_gsl<-r_gsl_odeiv2(modelName, as.double(outputTimes), y0, params_inputs)
+    
+    yy_gsl_as_list <- mclapply(seq(dim(yy_gsl)[3]), function(x) yy_gsl[ , , x], mc.preschedule = FALSE, mc.cores = mc.cores)
+    output_yy <- mclapply(1:N, function(i)  apply(yy_gsl_as_list[[i]],2,outputFunctions_list[[i]]), mc.preschedule = FALSE, mc.cores = mc.cores)
+  }
+  else
+  {
+    if(environment!="R")
+    {
+      str <- sprintf("%s is not admitted as value for the variable 'environment'. The default value 'R' will be used instead.", environment)
+      warning(str)
+    }
+    source(sprintf("%s.R",modelName))
+    func <- eval(as.name(modelName))
+    numSimulations <- dim(params_inputs)[2]
+    yy <- mclapply(1:numSimulations, function(i) matrix(t(lsode(y0[,i], c(0,outputTimes_list[[i]]), func=func, parms=params_inputs[,i])[-1, -1]),ncol=length(outputTimes_list[[i]])), mc.preschedule = FALSE, mc.cores = mc.cores)
+    output_yy <- mclapply(1:numSimulations, function(i)  apply(yy[[i]],2,outputFunctions_list[[i]]), mc.preschedule = FALSE, mc.cores = mc.cores)
+  }
+  return(output_yy)
+}
+
+
