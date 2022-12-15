df87c1f861fdaf9cce79a0756f6a44d63401c268
Andrei Kramer <andreikr@kth.se>
2022-12-15 12:52:57 +0100

    makeAcceptanceProbability is now documented and exported

--- UQ/R/runModel.R
+++ UQ/R/runModel.R
@@ -188,6 +188,18 @@ makeObjective <- function(experiments,modelName,distance,parMap=identity,mc.core
   return(Objective)
 }
 
+#' Ths function Creates an acceptanceProbability function
+#'
+#' The returned closure needs only the sampling variables (parABC) as
+#' inputand calculates a probability of accepting Markov chainmoves.
+#'
+#' @export
+#' @param experiments a list of experiments
+#' @param modelName an annotated string, with the model name and model file as comment
+#' @param getAcceptanceProbability an R function that mape the results of a simulation and experimental data to an acceptance probability
+#' @param parMap an optional mapping between sampling parameters (parABC) and model parameters (e.g. rescaling,re-ordering).
+#' @param mc.cores number of cores to use
+#' @return a function that calculates probabilities given only parABC as input; it implicitly uses all the argiments to this function.
 makeAcceptanceProbability <- function(experiments, modelName, getAcceptanceProbability, parMap=identity, mc.cores=detectCores())
 {
   acceptanceProbability <- function(parABC){
