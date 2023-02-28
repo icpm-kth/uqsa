9672e909e53b67483303d0f3c4f6e0805d1ee133
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2023-02-28 13:47:34 +0100

    added a function called uqsa_example() which returns a path to the named example

---
+++ R/uqsa_example_data.R
@@ -0,0 +1,17 @@
+#' Load an example model for this package
+#'
+#' This function finds the path to an example model, given by name.
+#' In the SBtab format, model and data travel together (in different
+#' tables, but the same documents).
+#'
+#' @param modelName name of model, e.g.: "AKAR4", "AKAP79", "CaMKII";
+#'     if empty, this function lists all available examples.
+#' @return list of files that belong to the named example model
+#' @export
+uqsa_example<-function(modelName=NULL) {
+	if (is.null(modelName)) {
+		return(dir(system.file("extdata", package = "uqsa")))
+	} else {
+		return(system.file("extdata", modelName, package = "uqsa", mustWork = TRUE))
+	}
+}
