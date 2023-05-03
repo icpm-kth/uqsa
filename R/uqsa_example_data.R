7a879c66a86cad71920f0daa9946dc6e1ab7707b
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2023-05-03 16:26:48 +0200

    added a few tests

--- R/uqsa_example_data.R
+++ R/uqsa_example_data.R
@@ -11,6 +11,11 @@
 #'
 #' @param modelName name of model, e.g.: "AKAR4", "AKAP79", "CaMKII";
 #'     if empty, this function lists all available examples.
+#' @param full.names return full paths to files - defaults to TRUE
+#' @param pat pattern to find specific files; if NULL, this function
+#'     returns the directory of the example
+#' @param f alternatively, pat can be set to files ending in f [this
+#'     value]
 #' @return The location of the examples in the current environment if
 #'     called with no arguments, the paths to the model files if a
 #'     modelName was provided or the full path to the example if the
@@ -29,7 +34,10 @@
 #' > uqsa_example("AKAR4",pat=NULL)
 #'  [1] "/path/extdata/AKAR4"
 #' @export
-uqsa_example<-function(modelName=NULL,full.names=TRUE,pat='[.]tsv$') {
+uqsa_example<-function(modelName=NULL,full.names=TRUE,pat='[.]tsv$',f=NULL) {
+	if (!is.null(f)){
+		pat <- sprintf("[.]%s$",f)
+	}
 	if (is.null(modelName)) {
 		return(dir(system.file("extdata", package = "uqsa")))
 	} else if (!is.null(pat) && is.character(pat)) {
