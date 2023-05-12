8183ccd5a927b61c7912a27023be2c2dd033038d
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2023-05-12 18:22:34 +0200

    added a test for example data validity using the functions in SBtabVFGEN

--- tests/testthat/test-uqsa_example_data.R
+++ tests/testthat/test-uqsa_example_data.R
@@ -1,13 +1,23 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
+	x <- 9
+	expect_equal(3 * 3, x)
 })
 
-test_that("AKAR4 is findable", {
+test_that("Examples are findable", {
 	expect_vector(uqsa_example(),ptype=character())
 })
 
-test_that("AKAR4 tsv files are findable (f)", {
-	expect_vector(uqsa_example("AKAR4",f="tsv"),ptype=character())
+test_that("AKAR4 tsv files are findable (f) and valid", {
+	expect_vector(AKAR4.tsv <- uqsa_example("AKAR4",f="tsv"),ptype=character())
+	expect_vector(tab <- SBtabVFGEN::sbtab_from_tsv(AKAR4.tsv,verbose=FALSE))
+	expect_null(suppressMessages(SBtabVFGEN::sbtab.valid(tab)))
+})
+
+test_that("AKAP79 tsv files are findable (f) and valid", {
+	expect_vector(AKAP79.tsv <- uqsa_example("AKAP79",f="tsv"),ptype=character())
+	expect_vector(tab <- SBtabVFGEN::sbtab_from_tsv(AKAP79.tsv,verbose=FALSE))
+	expect_warning(suppressMessages(SBtabVFGEN::sbtab.valid(tab)))
 })
 
 test_that("AKAR4 c files are findable ", {
@@ -17,3 +27,4 @@ test_that("AKAR4 c files are findable ", {
 test_that("non-existing example fails",{
 	expect_error(uqsa_example("ABCDEF"))
 })
+
