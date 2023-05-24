73f10f08a84117e3c67a1c1b537866784e882d8c
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2023-05-24 13:28:45 +0200

    one more test

--- tests/testthat/test-sensitivity.R
+++ tests/testthat/test-sensitivity.R
@@ -15,3 +15,15 @@ test_that("sum.of.bin.variance functions work", {
 	expect_lt(max(V),1.0)
 	expect_gt(min(V),0.0)
 })
+
+test_that("sensitivity function works",{
+	X <- c(rnorm(100,sd=1),7) # 7>3*sd to have at least a few NA values in binMeansY
+	ny <- 2
+	Y <- matrix(c(sin(X),cos(X)),ncol=ny)
+	S <- sensitivity(as.matrix(X),Y,nBins=30)
+	expect_type(S,"double")
+	expect_true(is.matrix(S))
+	expect_equal(dim(S),c(2,1))
+	expect_lt(max(S),1.0)
+	expect_gt(min(S),0.0)
+})
