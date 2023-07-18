56027294024c706f3c8c8536f4c23e23f8311d48
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2023-07-18 17:54:01 +0200

    a distance function that quits early if any simulated value is NA (the simulation failed for at least one experiment)

--- inst/extdata/CaMKII/runABCMCMC_CaMKII.R
+++ inst/extdata/CaMKII/runABCMCMC_CaMKII.R
@@ -42,7 +42,7 @@ parVal <- model$par()[1:numPar]
 # we sample in logarithmic space, so the model get's a transfornmation
 # function to compensate:
 parMap <- function(parABC){
-  return(10^parABC)
+	return(10^parABC)
 }
 
 ## scale to determine prior values
@@ -54,12 +54,12 @@ ul <- log10(parVal*defRange)
 
 ## Define the experiments that have to be considered in each iteration of the for loop to compare simulations with experimental data
 experimentsIndices <- list(
-  which(startsWith(names(experiments),"E0")),
-  which(startsWith(names(experiments),"E1")),
-  which(startsWith(names(experiments),"E2")),
-  which(startsWith(names(experiments),"E3")),
-  which(startsWith(names(experiments),"E4")),
-  which(startsWith(names(experiments),"E5"))
+ which(startsWith(names(experiments),"E0")),
+ which(startsWith(names(experiments),"E1")),
+ which(startsWith(names(experiments),"E2")),
+ which(startsWith(names(experiments),"E3")),
+ which(startsWith(names(experiments),"E4")),
+ which(startsWith(names(experiments),"E5"))
 )
 
 
@@ -68,97 +68,102 @@ ns <- 50000 # Size of the sub-sample from each chain
 npc <- 50000 # pre-calibration sample size
 
 # Define ABC-MCMC Settings
-delta <- 0.5
+delta <- 1.0
 
 set.seed(7619201)
 
-distanceMeasure <- function(yy_sim, yy_exp=Inf, yy_expErr=Inf){
-  distance <- mean(abs((yy_sim-as.matrix(yy_exp))/as.matrix(yy_expErr)), na.rm=TRUE)
-  return(distance)
+distanceMeasure <- function(funcSim, dataExpr=Inf, dataErr=Inf){
+	if (all(is.finite(funcSim))){
+		distance <- mean(abs((funcSim-as.matrix(dataExpr))/as.matrix(dataErr)), na.rm=TRUE)
+	} else {
+		distance <- Inf
+	}
+	return(distance)
 }
 
 save_sample <- function(ind,ABCMCMCoutput){
-  I <- paste(ind, collapse="_")
-  timeStr <- Sys.time()
-  timeStr <- gsub(":","_", timeStr)
-  timeStr <- gsub(" ","_", timeStr)
-  if (!dir.exists("./PosteriorSamples")) {
-    dir.create("./PosteriorSamples")
-  }
-  outFileR <- paste("./PosteriorSamples/Draws",modelName,"nChains",nChains,"ns",ns,"npc",npc,I,timeStr,".RData",collapse="_",sep="_")
-  save(ABCMCMCoutput, file=outFileR)
+	I <- paste(ind, collapse="_")
+	timeStr <- Sys.time()
+	timeStr <- gsub(":","_", timeStr)
+	timeStr <- gsub(" ","_", timeStr)
+	if (!dir.exists("./PosteriorSamples")) {
+		dir.create("./PosteriorSamples")
+	}
+	outFileR <- paste("./PosteriorSamples/Draws",modelName,"nChains",nChains,"ns",ns,"npc",npc,I,timeStr,".RData",collapse="_",sep="_")
+	save(ABCMCMCoutput, file=outFileR)
 }
 
 
 start_time = Sys.time()
 for (i in 1:length(experimentsIndices)){
-  expInd <- experimentsIndices[[i]]
-  objectiveFunction <- makeObjective(experiments[expInd], modelName, distanceMeasure, parMap)
-  acceptanceProbability <- makeAcceptanceProbability(experiments[expInd], modelName, getAcceptanceProbability, parMap)
-  
-  cat("#####Starting run for Experiments ", expInd, "######\n")
-  ## If First Experimental Setting, Create an Independente Colupla
-  if(i==1){
-    message("- Initial Prior: uniform product distribution")
-    rprior <- rUniformPrior(ll, ul)
-    dprior <- dUniformPrior(ll, ul)
-    ## Otherwise, Take Copula from the Previous Exp Setting and Use as a Prior
-  } else {
-    start_time_fitCopula <- Sys.time()
-    message("- New Prior: fitting Copula based on previous MCMC runs")
-    C <- fitCopula(ABCMCMCoutput$draws)
-    rprior <- rCopulaPrior(C)
-    dprior <- dCopulaPrior(C)
-    cat("\nFitting copula:")
-    print(Sys.time()-start_time_fitCopula)
-  }
-  
-  ## Run Pre-Calibration Sampling
-  message("- Precalibration")
-  start_time_preCalibration <- Sys.time()
-  pC <- preCalibration(objectiveFunction, npc, rprior)
-  cat("\nPreCalibration:")
-  print(Sys.time()-start_time_preCalibration)
-  
-  ## Get Starting Parameters from Pre-Calibration
-  M <- getMCMCPar(pC$prePar, pC$preDelta, delta=delta, num = nChains)
-  M$startPar <- matrix(M$startPar, nChains)
-  for(j in 1 : nChains){
-    stopifnot(dprior(M$startPar[j,])>0)
-    cat("Chain", j, "\n")
-    cat("\tMin distance of starting parameter for chain",j," = ", min(objectiveFunction(M$startPar[j,])),"\n")
-    cat("\tMean distance of starting parameter for chain",j," = ", mean(objectiveFunction(M$startPar[j,])),"\n")
-    cat("\tMax distance of starting parameter for chain",j," = ", max(objectiveFunction(M$startPar[j,])),"\n")
-  }
-  ## Run ABC-MCMC Sampling
-  cat(sprintf("-Running MCMC chains \n"))
-  start_time_ABC = Sys.time()
-  cl <- makeForkCluster(nChains)
-  clusterExport(cl, c("objectiveFunction", "M", "ns", "delta", "dprior", "acceptanceProbability"))
-  out_ABCMCMC <- parLapply(cl, 1:nChains, function(j) ABCMCMC(objectiveFunction, M$startPar[j,], ns, M$Sigma, delta, dprior, objectiveFunction))
-  stopCluster(cl)
-  
-  ABCMCMCoutput <- do.call(Map, c(rbind,out_ABCMCMC))
-  if (!is.matrix(ABCMCMCoutput$draws))
-    stop("All chains got stuck.")
-  
-  if (!is.null(ABCMCMCoutput$scores)){
-    ABCMCMCoutput$scores <- as.numeric(t(ABCMCMCoutput$scores))
-  }
-  end_time = Sys.time()
-  time_ = end_time - start_time_ABC
-  cat("\nABCMCMC for experimental set",i,":")
-  print(time_)
-  cat("\nRegularizations:", ABCMCMCoutput$nRegularizations)
-  cat("\nAcceptance rate:", ABCMCMCoutput$acceptanceRate)
-  
-  cat("\n-Saving sample \n")
-  save_sample(experimentsIndices[[1]],ABCMCMCoutput)
-  cat("memory usage: \n")
-  print(gc())
+	expInd <- experimentsIndices[[i]]
+	objectiveFunction <- makeObjective(experiments[expInd], modelName, distanceMeasure, parMap)
+	acceptanceProbability <- makeAcceptanceProbability(experiments[expInd], modelName, getAcceptanceProbability, parMap)
+
+	cat("#####Starting run for Experiments ", expInd, "######\n")
+	## If First Experimental Setting, Create an Independente Colupla
+	if(i==1){
+		message("- Initial Prior: uniform product distribution")
+		rprior <- rUniformPrior(ll, ul)
+		dprior <- dUniformPrior(ll, ul)
+		## Otherwise, Take Copula from the Previous Exp Setting and Use as a Prior
+	} else {
+		start_time_fitCopula <- Sys.time()
+		message("- New Prior: fitting Copula based on previous MCMC runs")
+		C <- fitCopula(ABCMCMCoutput$draws)
+		rprior <- rCopulaPrior(C)
+		dprior <- dCopulaPrior(C)
+		cat("\nFitting copula:")
+		print(Sys.time()-start_time_fitCopula)
+	}
+
+	## Run Pre-Calibration Sampling
+	message("- Precalibration")
+	start_time_preCalibration <- Sys.time()
+	pC <- preCalibration(objectiveFunction, npc, rprior)
+	cat("\nPreCalibration:")
+	print(Sys.time()-start_time_preCalibration)
+
+	## Get Starting Parameters from Pre-Calibration
+	M <- getMCMCPar(pC$prePar, pC$preDelta, delta=delta, num = nChains)
+	M$startPar <- matrix(M$startPar, nChains)
+	for(j in 1 : nChains){
+		stopifnot(dprior(M$startPar[j,])>0)
+		cat("Chain", j, "\n")
+		cat("\tMin distance of starting parameter for chain",j," = ", min(objectiveFunction(M$startPar[j,])),"\n")
+		cat("\tMean distance of starting parameter for chain",j," = ", mean(objectiveFunction(M$startPar[j,])),"\n")
+		cat("\tMax distance of starting parameter for chain",j," = ", max(objectiveFunction(M$startPar[j,])),"\n")
+	}
+	## Run ABC-MCMC Sampling
+	cat(sprintf("-Running MCMC chains \n"))
+	start_time_ABC = Sys.time()
+	cl <- makeForkCluster(nChains)
+	clusterExport(cl, c("objectiveFunction", "M", "ns", "delta", "dprior", "acceptanceProbability"))
+	out_ABCMCMC <- parLapply(cl, 1:nChains, function(j) ABCMCMC(objectiveFunction, M$startPar[j,], ns, M$Sigma, delta, dprior, objectiveFunction))
+	stopCluster(cl)
+
+	ABCMCMCoutput <- do.call(Map, c(rbind,out_ABCMCMC))
+	if (!is.matrix(ABCMCMCoutput$draws))
+		stop("All chains got stuck.")
+
+	if (!is.null(ABCMCMCoutput$scores)){
+		ABCMCMCoutput$scores <- as.numeric(t(ABCMCMCoutput$scores))
+	}
+	end_time = Sys.time()
+	time_ = end_time - start_time_ABC
+	cat("\nABCMCMC for experimental set",i,":")
+	print(time_)
+	cat("\nRegularizations:", ABCMCMCoutput$nRegularizations)
+	cat("\nAcceptance rate:", ABCMCMCoutput$acceptanceRate)
+
+	cat("\n-Saving sample \n")
+	save_sample(experimentsIndices[[1]],ABCMCMCoutput)
+	cat("memory usage: \n")
+	print(gc())
 }
 end_time = Sys.time()
 time_ = end_time - start_time
 cat("\nTotal time:")
 print(time_)
+
 
