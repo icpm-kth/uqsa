84f89d6aebf286692c43d4773fcd42a977376001
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2023-08-14 18:25:01 +0200

    fixed the manual of the plot functions (subject to revision anyway)

--- R/plotSimulations.R
+++ R/plotSimulations.R
@@ -1,22 +1,21 @@
 # Simulate a model imported from SBtab and plot simulations and experimental data, given a parameter vector in input
 # Copyright (C) 2023 Federica Milinanni (fedmil@kth.se)
 
-#'  Simulate a model imported from SBtab and plot simulations and experimental data, given a parameter vector in input
-#'  
-#'  This function imports model and experimental data saved as SBtab files,
-#'  it simualtes the model with the same initial conditions and input as in
-#'  the corresponding experiments, and it plots the simulations together with
-#'  the corresponding experimental data.
-#'  It currently works with one dimensional output.
-#'  
-#'  @export
-#'  @param SBtabDir the directory that contains `.tsv` files (with SBtab content)
-#'  @param paramVal parameter vector with which the model has to be simulated
-#'  @param plotDir the directory where the plots will be saved as .pdf and .RData variables
-#'  @param width width of the plot window
-#'  @param heigth heigth of the plot window
-#'  @return a vector of R plot objects
-
+#' Simulate and plot Data and Simulation
+#'
+#' This function imports model and experimental data saved as SBtab files,
+#' it simualtes the model with the same initial conditions and input as in
+#' the corresponding experiments, and it plots the simulations together with
+#' the corresponding experimental data.
+#' It currently works with one dimensional output.
+#'
+#' @export
+#' @param SBtabDir the directory that contains `.tsv` files (with SBtab content)
+#' @param paramVal parameter vector with which the model has to be simulated
+#' @param plotDir the directory where the plots will be saved as .pdf and .RData variables
+#' @param width width of the plot window
+#' @param heigth heigth of the plot window
+#' @return a vector of R plot objects
 plotSimualtionsFromSBtab <- function(SBtabDir, paramVal, plotDir = NULL, width = 15, heigth = 10){
   require(SBtabVFGEN)
   require(rgsl)
@@ -24,7 +23,7 @@ plotSimualtionsFromSBtab <- function(SBtabDir, paramVal, plotDir = NULL, width =
   require(deSolve)
   require(reshape2)
   require(ggplot2)
-  
+
   model = import_from_SBtab(SBtabDir)
   #setwd(SBtabDir)
   rFile<-paste0(SBtabDir,'/',comment(model),'.R')
@@ -33,44 +32,44 @@ plotSimualtionsFromSBtab <- function(SBtabDir, paramVal, plotDir = NULL, width =
   #modelName <- checkModel(comment(model),rFile)  # uncomment to use deSolve as backend
   #source(rFile)
   experiments <- import_experiments(modelName, SBtabDir)
-  
+
   paramNames <- model[["Parameter"]][["!Name"]]
   names(paramVal)<-paramNames
-  
+
   # This function maps parameter variables from logarithmic to linear scale
   parMap <- function(parABC) {
     return(10^parABC)
   }
-  
-  # Simualte the model 
+
+  # Simualte the model
   output_yy <- runModel(experiments, modelName, paramVal, parMap = parMap)
-  
+
   # Create folder for plots (if not provided in input)
   if(is.null(plotDir)){
     plotDir <- paste0(SBtabDir,"/../plots",modelName)
     if(!dir.exists(plotDir))
       dir.create(plotDir)
   }
-  
-  plots <- list() 
-  for(i in 1:length(experiments)){    
-    
+
+  plots <- list()
+  for(i in 1:length(experiments)){
+
     # Save the output of the simulation as a data frame with time and output value
     df <- as.data.frame(x = list(output_yy[[i]][["func"]][1,,1], experiments[[1]][["outputTimes"]]), col.names = c("y","t"))
-    
+
     # Save also the experimental data as a dataframe
     yy_exp <- experiments[[i]][["outputValues"]]
     dfExp <- data.frame(t = experiments[[i]][["outputTimes"]],y = yy_exp)
-    
+
     pl <- ggplot(df, aes(x = t, y = y)) + geom_line(color = "blue") + geom_point(data = na.omit(dfExp), aes(x = t, y = y)) + ggtitle(model$Experiments[["!Name"]])
     print(pl)
     ggsave(paste0(plotDir,"/",modelName,"_PlotExperimentAndSimulation_",model$Experiments[["!Name"]][i],".pdf"), plot = pl, width = width, height = heigth, units = "cm")
-    
+
     plots[[i]] <- pl
-    
+
     # lengthState <- dim(output_yy[[i]][["state"]])[1]
     # nrows <- ceil(sqrt(lengthState))
-    
+
     # gtable()
     # par(mfrow=c(nrows,nrows))
     # print(pl)
@@ -80,9 +79,9 @@ plotSimualtionsFromSBtab <- function(SBtabDir, paramVal, plotDir = NULL, width =
     #   print(plState)
     # }
   }
-  
+
   save(plots, file = paste0(plotDir,"/",modelName,"_PlotsExperimentsAndSimulations.RData"))
-  
+
   return(plots)
 }
 
@@ -95,8 +94,8 @@ plotSimualtionsFromSBtab <- function(SBtabDir, paramVal, plotDir = NULL, width =
 # 1.180435e+00, -4.999689e+00, -6.042806e-01, -1.305900e+00, -4.115583e-01,
 # 2.965258e+00, -1.669173e+00, -9.119168e-01,  1.018882e+00,  1.933725e+00,
 # -6.643307e-02,  2.061177e-02)
-# 
+#
 # SBtabDir <- # path to the directory with the SBtab files
-# 
+#
 # plots <- plotSimualtionsFromSBtab(SBtabDir, paramVal)
-# 
+#
