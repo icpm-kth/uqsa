6d9a4e6d723141d6ddd3e83ecf26cb5f11d33c66
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2024-09-09 19:09:33 +0200

    re-worked the ggplotTimeSeriesStates function to eventually replace the simpler function

--- R/plotSimulationsAndData.R
+++ R/plotSimulationsAndData.R
@@ -13,14 +13,20 @@
 #' @return list of plots with simulations and experimental data
 ggplotTimeSeries <- function(simulations, experiments, nrow=NULL, ncol=NULL, plot.state=FALSE){
 	num.experiments <- length(simulations)
-	N <- dim(simulations[[1]]$func)[3]
+
 	stopifnot(length(simulations) == length(experiments))
 	num.out.funcs <- NCOL(experiments[[1]]$outputValues)
 	p <- list()
 	for(i in seq(length(experiments))){
+		N <- dim(simulations[[i]]$func)[3]
 		oNames <- names(experiments[[i]][["outputValues"]])
+		if ("measurementTimes" %in% names(experiments[[i]])){
+			t_ <- experiments[[i]][["measurementTimes"]]
+		} else {
+			t_ <- experiments[[i]][["outputTimes"]]
+		}
 		for(j in seq(num.out.funcs)){
-			df.experiments <- data.frame(t=experiments[[i]][["outputTimes"]], y=experiments[[i]][["outputValues"]][[j]])
+			df.experiments <- data.frame(t=t_, y=experiments[[i]][["outputValues"]][[j]])
 			y <- as.numeric(simulations[[i]]$func[j,,])
 			df.simulations <- data.frame(t=rep(experiments[[i]][["outputTimes"]],N), y=y, sim=rep(seq(N),each=length(experiments[[i]][["outputTimes"]])))
 			p[[i]] <-
@@ -53,14 +59,18 @@ ggplotTimeSeries <- function(simulations, experiments, nrow=NULL, ncol=NULL, plo
 #' @param show.plot boolean variable. Set show.plot=TRUE to display plots
 #'      when running the funcion, FALSE otherwise
 #' @return list of plots with simulations and experimental data
-ggplotTimeSeriesStates <- function(simulations, experiments, nrow=NULL, ncol=NULL, var.names=NULL, type="boxes"){
+ggplotTimeSeriesStates <- function(simulations, experiments, var.names=NULL, type="boxes", plot.states=TRUE,ttf=identity){
 	num.experiments <- length(experiments)
 	stopifnot(num.experiments == length(simulations))
 	num.of.funcs <- NCOL(experiments[[1]]$outputValues)
 	num.of.vars <- NROW(simulations[[1]]$state)
-	N <- dim(simulations[[1]]$func)[3]
+
 	p <- list()
-	M <- (num.of.funcs+num.of.vars)
+	if (plot.states){
+		M <- (num.of.funcs+num.of.vars)
+	} else {
+		M <- (num.of.funcs)
+	}
 	T1 <- theme(plot.title=element_text(size=rel(2)),
 	            axis.text=element_text(size=rel(1.5)),
 	            axis.title=element_text(size=rel(1.6)))
@@ -71,6 +81,7 @@ ggplotTimeSeriesStates <- function(simulations, experiments, nrow=NULL, ncol=NUL
 	}
 
 	for(i in seq(length(experiments))){
+		N <- dim(simulations[[i]]$func)[3]
 		oNames <- names(experiments[[i]][["outputValues"]])
 		if (is.null(var.names) && "initialState" %in% names(experiments[[i]])){
 			xNames <- names(experiments[[i]][["initialState"]])
@@ -80,32 +91,37 @@ ggplotTimeSeriesStates <- function(simulations, experiments, nrow=NULL, ncol=NUL
 			xNames <- var.names
 		}
 		for(j in seq(num.of.funcs)){
-			y <- experiments[[i]][["outputValues"]][[j]]
-			y_upper <- y + experiments[[i]][["errorValues"]][[j]]
-			y_lower <- y - experiments[[i]][["errorValues"]][[j]]
-			df.experiments <- data.frame(t=experiments[[i]][["outputTimes"]], y=y, upper=y_upper, lower=y_lower)
-			y <- as.numeric(simulations[[i]]$func[j,,])
-			df.simulations <- data.frame(t=experiments[[i]][["outputTimes"]], y=y, sim=rep(seq(N),each=length(experiments[[i]][["outputTimes"]])))
+			z <- experiments[[i]][["outputValues"]][[j]]
+			dz <- experiments[[i]][["errorValues"]][[j]]
+			if ("measurementTimes" %in% names(experiments[[i]])){
+				t_ <- ttf(experiments[[i]][["measurementTimes"]])
+			} else {
+				t_ <- ttf(experiments[[i]][["outputTimes"]])
+			}
+			df.experiments <- data.frame(t=t_, y=z, upper=z+dz, lower=z-dz)
+			f <- as.numeric(simulations[[i]]$func[j,,])
+			Q <- quantile(c(z,f),probs=c(0.1,0.5,0.9),na.rm=TRUE)
+			tf <- ttf(experiments[[i]][["outputTimes"]])
+			df.simulations <- data.frame(t=rep(tf,N), y=f, sim=rep(seq(N),each=length(tf)))
 			p[[(i-1)*M+j]] <- ggplot2::ggplot(df.simulations)+g+
 				ggplot2::geom_errorbar(data=df.experiments, aes(x=t, y=y, ymin = lower, ymax = upper, color="red"), inherit.aes=FALSE)+
 				ggplot2::ggtitle(names(experiments[i]))+T1+
-				ggplot2::labs(y=oNames[j])
+				ggplot2::labs(y=oNames[j])+ylim(Q[1],Q[3])
 		}
-		for(j in seq(num.of.vars)){
-			y <- as.numeric(simulations[[i]]$state[j,,])
-			df.simulations <- data.frame(t=rep(experiments[[i]][["outputTimes"]],N), y=y, sim=rep(seq(N), each=length(experiments[[i]][["outputTimes"]])))
-			p[[(i-1)*M+j+num.of.funcs]] <- ggplot2::ggplot(df.simulations,aes(x=t, y=y, group=sim))+g+
-				ggplot2::ggtitle(names(experiments[i]))+T1+
-				ggplot2::labs(y=xNames[j])
+		if (plot.states){
+			for(j in seq(num.of.vars)){
+				y <- as.numeric(simulations[[i]]$state[j,,])
+				ty <- ttf(experiments[[i]][["outputTimes"]])
+				df.simulations <- data.frame(t=rep(ty,N), y=y, sim=rep(seq(N), each=length(ty)))
+				p[[(i-1)*M+j+num.of.funcs]] <- ggplot2::ggplot(df.simulations,aes(x=t, y=y, group=sim))+g+
+					ggplot2::ggtitle(names(experiments[i]))+T1+
+					ggplot2::labs(y=xNames[j])
+			}
 		}
 	}
-	if (is.null(nrow)){
-		nrow <- num.of.funcs+num.of.vars
-	}
-	if (is.null(ncol)){
-		ncol <- num.experiments
-	}
-	return(gridExtra::marrangeGrob(grobs=p,ncol=ncol,nrow=nrow))
+	n <- M
+	m <- num.experiments
+	return(gridExtra::marrangeGrob(grobs=p,ncol=m,nrow=n))
 }
 
 plotTimeSeriesBase <- function(simulations, experiments, nmax=NULL){
