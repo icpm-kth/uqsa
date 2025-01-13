85e738b0b969be172c5420afbeb3007d7a793521
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2025-01-13 20:08:19 +0100

    Code generation seems to work for one of our models. It is not tested yet with all kinds of models. At the moment, only the Ryacas backend is available for derivatives.

--- R/ode.R
+++ R/ode.R
@@ -38,40 +38,10 @@ yJacobian <- function(f,x){
 	x <- yacasMath(x)
 	F <- paste(f,collapse=',')
 	X <- paste(x,collapse=',')
-	J <- yac_str(y_fn(sprintf("{%s},{%s}",F,X),"JacobianMatrix"))
+	J <- Ryacas::yac_str(Ryacas::y_fn(sprintf("{%s},{%s}",F,X),"JacobianMatrix"))
 	J <- gsub("[{}]","",J)
 	J <- matrix(unlist(strsplit(J,",")),nrow=length(f),ncol=length(x),byrow=TRUE)
 	return(yacasMath(J,rev=TRUE))
-}
-
-#' Make math C compatible
-#'
-#' Given a character array with math expressions, return a similar
-#' vector, with c-compatible math. The most important change is
-#' related to powers.
-#'
-#' @param v a character vector with math expressions
-#' @param gsl use power functions from the GNU scientific library
-#' @return a vector with c-compatible math
-#' @export
-cMath <- function(v,gsl=TRUE){
-	# various integer powers
-	v <- gsub("\\(([^()]+)\\)\\^\\(([[:digit:]]+[.][eE]?[-+]?[[:digit:]]+)\\)","pow(\\1,\\2)",v)
-	v <- gsub("\\<([[:alnum:]]+)\\>\\^\\(([[:digit:]]+[.][eE]?[-+]?[[:digit:]]+)\\)","pow(\\1,\\2)",v)
-	v <- gsub("\\<([[:alnum:]]+)\\>\\^([[:digit:]]+[.][eE]?[-+]?[[:digit:]]+)\\>","pow(\\1,\\2)",v)
-	if (gsl){
-		v <- gsub("\\(([^()]+)\\)\\^([2-9])","gsl_pow_\\2(\\1)",v)
-		v <- gsub("\\b(\\w+)\\b\\^([2-9])","gsl_pow_\\2(\\1)",v)
-		v <- gsub("\\b(\\d+)\\b\\^([2-9])","gsl_pow_\\2(\\1)",v)
-		v <- gsub("\\(([^()]+)\\)\\^([0-9]+)","gsl_pow_int(\\1,\\2)",v)
-		v <- gsub("\\b(\\w+)\\b\\^([0-9]+)","gsl_pow_int(\\1,\\2)",v)
-		v <- gsub("\\b(\\d+)\\b\\^([0-9]+)","gsl_pow_int(\\1,\\2)",v)
-	}
-	v <- gsub("\\(([^()]+)\\)\\^\\(([^()]+)\\)","pow(\\1,\\2)",v)
-	v <- gsub("\\b(\\w+)\\b\\^\\(([^()]+)\\)","pow(\\1,\\2)",v)
-	v <- gsub("\\b(\\d+)\\b\\^\\(([^()]+)\\)","pow(\\1,\\2)",v)
-	v <- gsub("\\(([[:alnum:]]+)\\)\\^\b([[:alnum:]]+)\b","pow(\\1,\\2)",v)
-	return(v)
 }
 
 #' replace_powers
@@ -80,6 +50,142 @@ cMath <- function(v,gsl=TRUE){
 #' @param v a character vector
 #' @useDynLib uqsa, replace_pow=replace_pow
 replace_powers <- function(v){
-	w<-.Call(replace_pow,v)
+	w<-.Call(replace_pow,as.character(v))
 	return(w)
+}
+
+writeComment <- \(x) {
+	n <- pmax(1,1+max(nchar(x))-nchar(x))
+	return(sprintf("/* %s %*s */",x,n," "))
+}
+
+ch <- \(d) {
+	w <- as.character(d[[2]])
+	names(w) <- d[[1]]
+	return(w)
+}
+
+cOffset <- \(d) {seq(0,NROW(d)-1)}
+
+#' Write C code
+#' @param ode a list of data.frames with the text representation of the ordinary differential equation
+#' @export
+#' @return a character vector with the generated code, one element is one line.
+generateCode <- function(ode){
+	modelName <- comment(ode) %otherwise% "model"
+
+	# simplify a data.frame with two columns to a named character vector, assuming it's name/value pairs
+	makeEnum <- \(var,enumName, lastEntry) {
+		sprintf("enum %s { %s, %s };",enumName,paste('_',var,sep='', collapse=', '),lastEntry)
+	}
+	writeFunc <- \(fName,retValue,body,len=length(body),Rest=NULL){
+		if (length(retValue)>0) ret <- paste("double *",retValue,sep="", collapse=", ",recycle0=TRUE)
+		else ret <- NULL
+		arguments <- paste(c("double t","const double y_[]",ret,"void *par",Rest),sep=", ",collapse=", ")
+		n <- pmax(5,50 - nchar(ode$par[[1]])*2)
+		m <- pmax(5,50 - nchar(ode$var[[1]])*2)
+		return(c(
+			sprintf("int %s_%s(%s)",modelName,fName,arguments),
+			sprintf("\tdouble *p_=par;"),
+			sprintf("\tif (!y_ || !%s_) return %i;",retValue[1],len),
+			sprintf("\tdouble %s = %g;",ode$const[[1]],ode$const[[2]]),
+			sprintf("\tdouble %s = p_[_%s]; %*s /* [%3i] */",ode$par[[1]],ode$par[[1]],n," ",cOffset(ode$par)),
+			sprintf("\tdouble %s = y_[_%s]; %*s /* [%3i] */",ode$var[[1]],ode$var[[1]],m," ",cOffset(ode$var)),
+			sprintf("\tdouble %s = %s;",ode$exp[[1]],replace_powers(ode$exp[[2]])),
+			sprintf("\tmemset(%s,0,sizeof(double)*%i); ",retValue[1],len),
+			body,
+			sprintf("\treturn GSL_SUCCESS;\n}")
+		))
+	}
+
+	h <- c('stdlib','math','string','gsl/gsl_errno','gsl/gsl_odeiv2','gsl/gsl_math.h')
+	C <- c(sprintf("#include <%s.h>",h),"",
+	writeComment("Enums will be used for indexing purposes."),
+	makeEnum(ode$vf[[1]],'stateVariable','numStateVar'),
+	makeEnum(ode$par[[1]],'param','numParam'),
+	makeEnum(ode$func[[1]],'func','numFunc'),
+	makeEnum(ode$events[[1]],'eventLabel','numEvents'),"") # ens with a blank line
+
+	C <- c(C,writeComment(
+		c("The error codes indicate how many values a function returns.",
+			"Each function expects the output buffer to be allocated with at least that many values")))
+	# vf function
+	C <- c(C,writeComment("ODE vector field: y'f(t,y;p)"))
+	C <- c(C,writeFunc("vf","f_",sprintf("\tf_[_%s] = %s;",ode$vf[[1]],replace_powers(ode$vf[[2]]))))
+	x <- ch(ode$exp)
+	for (i in seq_along(x)){
+		CMD <- sprintf("%s := %s",names(x)[i],x[i])
+		Ryacas::yac_silent(yacasMath(CMD))
+	}
+	# Jacobian
+	C <- c(C,"",writeComment("ODE Jacobian: df(t,y;p)/dy"))
+	J <- replace_powers(yJacobian(ode$vf[[2]],ode$var[[1]]))
+	z <- grepl("^0$",J)
+	C <- c(C,writeFunc("jac",c("jac_","dfdt_"),sprintf("\tjac_[%i] = %s;",cOffset(J)[!z],J[!z]),length(J)))
+	# parameter Jacobian
+	C <- c(C,"",writeComment("ODE parameter Jacobian: df(t,y;p)/dp"))
+	Jp <- replace_powers(yJacobian(ode$vf[[2]],ode$par[[1]]))
+	z <- grepl("^0$",Jp)
+	C <- c(C,writeFunc("jacp",c("jacp_","dfdt_"),sprintf("\tjacp_[%i] = %s;",cOffset(Jp)[!z],Jp[!z]),length(Jp)))
+	# output function
+	C <- c(C,"",writeComment("Output Function (Observables)"))
+	C <- c(C,writeFunc("func","func_",sprintf("\tfunc_[_%s] = %s;",ode$func[[1]],ode$func[[2]])))
+	# event function
+	if ("tf" %in% names(ode)){
+		C <- c(C,"",writeComment(c("Scheduled Events","EventLabel specifies which transformation to apply.","The scalar dose variable can be used in the transformation (on the right)")))
+		v <- ode$tf[[2]]=="var"
+		Effect <- \(ev){
+			l <- ode$tf[[1]]==ev
+			y <- ch(ode$tf[l & v,c(3,4)])
+			p <- ch(ode$tf[l & !v,c(3,4)])
+			return(
+				c(sprintf("\tcase %s:",ev),
+					sprintf("\t\ty_[_%s] = %s",names(y),y),
+					sprintf("\t\tp_[_%s] = %s",names(p),p),
+					sprintf("\tbreak;")))
+		}
+		for (ev in unique(ode$tf[[1]])){
+		}
+		evsw <- c(
+		 sprintf("\tswitch(eventLabel){"),
+		 unlist(lapply(unique(ode$tf[[1]]),Effect)),
+		 "\t}"
+		)
+		C <- c(C,writeFunc("event",NULL,evsw,Rest="int eventLabel, double dose"))
+	}
+	return(C)
+}
+
+#' Load an ODE model from a file
+#'
+#' The input can be either a list of files, or one compressed archive
+#' file like "model.tar.gz". This function determines the name of the
+#' model from the name of the archive, or from the parent directory of
+#' uncompressed tsv files. This auto-determined name is attached as a
+#' comment of the return value. To override this choice, replace the
+#' comment.
+#'
+#' @param fileList list of file paths
+#' @export
+#' @return a list of data.frames describing the model
+loadODE <- function(fileList){
+	modelName <- basename(dirname(fileList[1]))
+	if (length(fileList)==1 && endsWith(basename(fileList),".tar.gz")) {
+		modelName <- sub("[.].*$","",basename(fileList[1]))
+		tartf <- untar(fileList,list=TRUE)
+		TMP="/dev/shm/ode_gen"
+		untar(fileList,exdir=TMP)
+		fileList <- paste0(TMP,'/',tartf)
+	}
+	rf <- \(f){
+		read.delim(file=f,header=FALSE)
+	}
+	ode <- lapply(fileList,rf)
+	Name <- tolower(sub("[.].*$","",basename(fileList)))
+	rename <- \(x) {
+		switch(x,statevariables="var",parameters="par",ode="vf",expressions="exp",outputfunctions="func",transformations="tf")
+	}
+	names(ode) <- lapply(Name,rename)
+	comment(ode) <- modelName
+	return(ode)
 }
--- src/match_pow.c
+++ src/match_pow.c
@@ -10,84 +10,155 @@ struct stack {
 	size_t n;
 };
 
-struct stack* stack_alloc(size_t N){
-	struct stack *s = malloc(sizeof(struct stack));
-	s->size = N;
-	s->n = 0;
-	s->buffer = malloc(sizeof(char)*s->size);
-	return s;
+#ifndef _GNU_SOURCE
+void *mempcpy(void *dest, const void *src, size_t len){
+  return memcpy (dest, src, len) + len;
+}
+#endif
+
+const char *pow_str[]={"gsl_pow_","gsl_pow_int","pow",NULL};
+enum pow_type {small_integer_power, integer_power, double_power};
+
+int word(char c){
+	if (c >= 'a' && c <= 'z') return 1;
+	if (c >= 'A' && c <= 'Z') return 1;
+	if (c >= '0' && c <= '9') return 1;
+	if (c=='_' || c=='.') return 1;
+	return 0;
 }
 
-void stack_free(struct stack **s){
-	free((*s)->buffer);
-	(*s)->n = 0;
-	(*s)->size = 0;
-	*s=NULL;
-}
-
-void push(struct stack *s, char val){
-	if (s==NULL) return;
-	if (s->n == s->size - 1){
-		s->size += 12;
-		s->buffer = realloc(s->buffer,s->size);
+int find(const char *s, char c){
+	int i;
+	for (i=0;i<strlen(s);i++){
+		if (s[i]==c) break;
 	}
-	s->buffer[s->n++] = val;
-	s->buffer[s->n] = '\0'; /* for safe printing */
+	return i;
 }
 
-char pop(struct stack *s){
-	char c='\0';
-	if (s->n > 0) {
-		c = s->buffer[--(s->n)];
-		s->buffer[s->n] = '\0';
+/* remove whitespace and one layer of parenthese*/
+void tighten(char **left, char **right){
+	while (**left==' ') (*left)++;
+	while (**right==' ') (*right)--;
+	if (**left=='(' && **right==')'){
+		(*left)++;
+		(*right)--;
 	}
-	return c;
 }
 
-int empty(struct stack *s){
-	return s->n == 0;
+/* does this: (var1 + (var2 / var3))         */
+/*            ^boundary            ^start    */
+/* either in forward or backward direction   */
+char* find_boundary(const char* src, char *start, int direction){
+	char *ptr=start;
+	if (direction==0) return ptr;
+	int n=0; /* parnethesis counter */
+	do {
+		switch(*ptr){
+		case ')': n-=direction; break;
+		case '(': n+=direction; break;
+		}
+		ptr+=direction;
+	} while (ptr>=src && (word(*ptr) || n>0));
+	return ptr-direction;
 }
 
-void stack_print(struct stack *s){
-	printf("«%s»\n",s->buffer);
+int count(const char *s, char c){
+	const char *p=s;
+	int z=0;
+	while (*p){
+		z+=(c==*p);
+		p++;
+	}
+	return z;
+}
+
+/* check that all n characters are numerical digits [0-9] */
+int digit(const char *str, size_t n){
+	int i;
+	for (i=0;i<n;i++){
+		if (str[i]<'0' || str[i]>'9') return 0;
+	}
+	return 1;
+}
+
+/* replaces a^b with pow(a,b) */
+void replace(char *buffer, const char *src){
+	char *ptr;
+	char *out;
+	char *b_open, *b_close, *e_open, *e_close;
+	/* b - base; e - exponent; */
+	const char *ps;
+	size_t n;
+	enum pow_type t=double_power;
+	char *hat=strchr(src,'^');
+	if (hat){
+		ptr = hat-1;
+		while (*ptr==' ') ptr--;
+		/* find base: */
+		b_close= ptr;
+		b_open = find_boundary(src,ptr,-1);
+		/* copy initial part, if any, to output buffer */
+		*(out=(char*) mempcpy(buffer,src,b_open-src))='\0';
+		/* find the exponent */
+		ptr  = hat+1;
+		while (*ptr==' ') ptr++;
+		e_open = ptr;
+		e_close= find_boundary(src,ptr,+1);
+		ptr=e_close+1;
+		tighten(&b_open,&b_close);
+		tighten(&e_open,&e_close);
+		/* add power functin to buffer: */
+		if (e_open==e_close && digit(e_open,1)){
+			t=small_integer_power;
+		} else if (digit(e_open,1+(e_close-e_open))) {
+			t=integer_power;
+		}
+		ps=pow_str[t];
+		n=strlen(ps);
+		switch (t){
+		case small_integer_power:
+			*(out=(char*) mempcpy(out,ps,n))='\0';
+			*(out=(char*) mempcpy(out,e_open,1+(e_close-e_open)))='\0';
+			*(out=(char*) mempcpy(out,"(",1))='\0';
+			*(out=(char*) mempcpy(out,b_open,1+(b_close-b_open)))='\0';
+			*(out=(char*) mempcpy(out,")",1))='\0';
+			break;
+		default:
+			*(out=(char*) mempcpy(out,ps,n))='\0';
+			*(out=(char*) mempcpy(out,"(",1))='\0';
+			*(out=(char*) mempcpy(out,b_open,1+(b_close-b_open)))='\0';
+			*(out=(char*) mempcpy(out,", ",2))='\0';
+			*(out=(char*) mempcpy(out,e_open,1+(e_close-e_open)))='\0';
+			*(out=(char*) mempcpy(out,")",1))='\0';
+		}
+		*(out=(char*) mempcpy(out,ptr,strlen(ptr)))='\0';
+	}
 }
 
 Rdata replace_pow(Rdata str){
-	const char *input = CHAR(STRING_ELT(str,0));
-	char *ptr = input;
-	char *hat; /* points to U+005E ^ CIRCUMFLEX ACCENT if found in str */
-	char *open=NULL, *close=NULL; /* first opening parenthesis related to ^ */
-	struct stack *s = stack_alloc(24);
-	Rdata w = PROTECT(NEW_CHARACTER(1));
-	char *out;
-	int n_prefix,n_base,n_exponent,n_suffix;
-	hat=strchr(ptr,'^');
-	if (hat) {
-		ptr = hat-1;
-		while (ptr > input && open==NULL){
-			switch (*ptr){
-			case ')': push(s,')'); break;
-			case '(': pop(s); break;
-			case '-':
-			case '+':
-			case '*':
-			case '/':
-			case ' ':
-				if (empty(s)){
-					open = ptr+1;
-				}
-			}
-			ptr--;
+	int i,j,N=length(str);
+	Rdata w = PROTECT(NEW_CHARACTER(N)); /* return value */
+	const char *input;
+	int n;
+	const char max_pow_str[]="gsl_pow_int(,)"; /* maximum possible extra content per caret (^) in input */
+	size_t max_l=strlen(max_pow_str);
+	char *buffer; /* output buffer */
+	char *src;    /* copy of the input */
+	int m;
+	for (i=0;i<N;i++){
+		input = CHAR(STRING_ELT(str,i));
+		n=count(input,'^');
+		m=1+strlen(input)+max_l*n;
+		buffer=malloc(m);
+		src=malloc(m);
+		strcpy(src,input);
+		strcpy(buffer,input);
+		for (j=0;j<n;j++){
+			replace(buffer,src);
+			strcpy(src,buffer);
 		}
-		n_base = hat - open;
-		n_exponent = close - hat;
-		n_prefix = open - input;
-		n_suffix = strlen(close);
-		out = malloc(n_prefix + n_base + n_exponent + n_suffix + 4);
-		memcpy(out,input,n_prefix);
+		SET_STRING_ELT(w, i, mkChar(buffer));
 	}
-	SET_STRING_ELT(w, 0, mkChar("a custom string constant"));
-	stack_free(&s);
 	UNPROTECT(1);
 	return w;
 }
