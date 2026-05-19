98b9e85dad251997d88074f59839c95ad74941d7
Andrei Kramer <andreikr@kth.se>
2026-05-19 15:16:01 +0200

    removed a mention of SBtabVFGEN in a test

--- tests/testthat/test-uqsa_example_data.R
+++ tests/testthat/test-uqsa_example_data.R
@@ -3,9 +3,8 @@ test_that("Examples are findable", {
 })
 
 test_that("AKAR4 tsv files are findable (f) and valid", {
-	expect_vector(AKAR4.tsv <- uqsa_example("AKAR4",f="tsv"),ptype=character())
+	expect_vector(AKAR4.tsv <- uqsa_example("AKAR4"),ptype=character())
 	expect_vector(tab <- model_from_tsv(AKAR4.tsv))
-	expect_true(suppressWarnings(suppressMessages(SBtabVFGEN::sbtab.valid(tab))))
 })
 
 test_that("non-existing example fails",{
