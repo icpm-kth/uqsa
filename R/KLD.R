fe716f1260f517a6e2b3bcf4900cd5d51896e3bc
Andrei Kramer <andreikr@kth.se>
2026-07-07 12:51:46 +0200

    updated KLD function to use several different options of density estimation

--- R/KLD.R
+++ R/KLD.R
@@ -1,38 +1,66 @@
-#' Kullback Leibler Divergence
-#'
-#' This function calculates the Kullback Leibler Divergence $D(P\|Q)$ value
-#' between two distributions $P$ and $Q$, represented by their two samples, `X` and
-#' `Y`. Both samples will have their density inferred.
-#' @param X sample from distribution P
-#' @param Y sample from distribution Q
-#' @return D, a scalar value, the Kullback Leibler Divergence
-#' @export
-KLD <- function(X,Y,use.kde=FALSE){
-	if (use.kde) { # quick and dirty KDE
-		density_fn <- function(ZETA, h=NULL) {
-			if (missing(h) || is.null(h)) {
-				SD <- apply(ZETA,2,sd)
-				iqr <- apply(ZETA,2,IQR)
-				ssize <- NROW(ZETA)
-				n <- NCOL(ZETA)
-				h <- 0.9*pmin(SD,iqr/1.34)*exp(-1/(2*n+1)*log(ssize)) # crude rule of thumb
-			}
-			return(
-				function(xi) { # p_Y(X)
-					sapply(
-						seq(NROW(xi)),
-						\(i) {
-							mean(mvtnorm::dmvnorm(ZETA, mean = xi[i, ], sigma = diag(h)))
-						}
-					)
+density_fn <- function(ZETA, h=NULL) {
+	# a very simplified KDE function
+	if (missing(h) || is.null(h)) {
+		stdv <- apply(ZETA,2,sd)
+		iqr <- apply(ZETA,2,IQR)
+		ssize <- NROW(ZETA)
+		n <- NCOL(ZETA)
+		h <- 0.9*pmin(stdv,iqr/1.34)*exp(-1/(2*n+1)*log(ssize)) # crude rule of thumb
+	}
+	return(
+		function(xi) { # p_Y(X)
+			sapply(
+				seq(NROW(xi)),
+				\(i) {
+					mean(mvtnorm::dmvnorm(ZETA, mean = xi[i, ], sigma = diag(h)))
 				}
 			)
 		}
+	)
+}
+
+
+#' Kullback Leibler Divergence
+#'
+#' This function calculates the Kullback Leibler Divergence $D(P\|Q)$
+#' value between two distributions $P$ and $Q$, represented by their
+#' two samples, `X` and `Y`. Both samples will have their density
+#' inferred. The intended use-case is to compare 2D and 3D densities,
+#' e.g.: to find interesting pairs of parameters within a bigger
+#' distribution.
+#'
+#' This estimate requires a method of density estimation, by default
+#' we use the copula based methods [fitCopula] and [dCopulaPrior]
+#' (which has a fairly high accuracy, but can be quite slow).
+#'
+#' Effects of setting `de` to
+#' - `"ks"`: density is estimated via `ks::kde()`
+#' - `"mclust"`: density is estimated via `mclust::dens()`
+#' - `"mvtnorm"`: density is estimated via manual kernel
+#'    density estimation, using the multivariate
+#'    Gaussian density `mvtnorm::dmvnorm`
+#'
+#' @param X sample from distribution P
+#' @param Y sample from distribution Q
+#' @param de density estimation mechanism (character scalar)
+#' @return D, a scalar value, the Kullback Leibler Divergence
+#' @export
+KLD <- function(X,Y,de=c("copula","ks","mclust","mvtnorm")){
+	de <- match.arg(de)
+	if (de == "mvtnorm" && requireNamespace("mvtnorm",quietly=TRUE)) { # quick and dirty KDE
 		P <- density_fn(X)
 		Q <- density_fn(Y)
+	} else if (de == "ks" && requireNamespace("ks",quietly=TRUE)) {
+		P <- \(xi) ks::kde(X, eval.points = xi, density=TRUE, binned=FALSE)$estimate
+		Q <- \(xi) ks::kde(Y, eval.points = xi, density=TRUE, binned=FALSE)$estimate
+	} else if (de == "mclust" && requireNamespace("mclust",quietly=TRUE)) {
+		MX <- mclust::Mclust(X)
+		MY <- mclust::Mclust(Y)
+		P <- \(xi) mclust::dens(xi,modelName=MX$modelName,parameters=MX$parameters)
+		Q <- \(xi) mclust::dens(xi,modelName=MY$modelName,parameters=MY$parameters)
 	} else {
-		P <- \(Z) apply(Z,1,dCopulaPrior(fitCopula(X))) # density estimate for X
-		Q <- \(Z) apply(Z,1,dCopulaPrior(fitCopula(Y))) # density estimate for Y
+		P <- dCopulaPrior(fitCopula(X)) # density estimate for X
+		Q <- dCopulaPrior(fitCopula(Y)) # density estimate for Y
 	}
 	D <- mean(log(P(X))-log(Q(X))) # X ~ P
 	return(D)
--- tests/testthat/test-KLD.R
+++ tests/testthat/test-KLD.R
@@ -1,4 +1,4 @@
-test_that("KLD for multivariate normal",{
+test_that("KLD for multivariate normal, with fitCopula",{
 	muA <- c(0,0)
 	SigmaA <- matrix(c(1,0.2,0.2,1),2,2)
 	muB <- c(0.1,-0.2)
@@ -9,6 +9,54 @@ test_that("KLD for multivariate normal",{
 		X <- MASS::mvrnorm(N,muA,SigmaA)
 		Y <- MASS::mvrnorm(N,muB,SigmaB)
 		D <- KLD(X,Y)
+		E <- exact_normal_kld(muA,SigmaA,muB,SigmaB)
+		expect_lt(abs(D-E),1e-2*length(muA))
+	}
+})
+
+test_that("KLD for multivariate normal, with mclust",{
+	muA <- c(0,0)
+	SigmaA <- matrix(c(1,0.2,0.2,1),2,2)
+	muB <- c(0.1,-0.2)
+	SigmaB <- matrix(c(1,0.4,0.4,1),2,2)
+	if (requireNamespace("MASS", quietly = TRUE)){
+		set.seed(42)
+		N <- 1e3
+		X <- MASS::mvrnorm(N,muA,SigmaA)
+		Y <- MASS::mvrnorm(N,muB,SigmaB)
+		D <- KLD(X,Y,de="mclust")
+		E <- exact_normal_kld(muA,SigmaA,muB,SigmaB)
+		expect_lt(abs(D-E),1e-2*length(muA))
+	}
+})
+
+test_that("KLD for multivariate normal, with ks",{
+	muA <- c(0,0)
+	SigmaA <- matrix(c(1,0.2,0.2,1),2,2)
+	muB <- c(0.1,-0.2)
+	SigmaB <- matrix(c(1,0.4,0.4,1),2,2)
+	if (requireNamespace("MASS", quietly = TRUE)){
+		set.seed(42)
+		N <- 1e4
+		X <- MASS::mvrnorm(N,muA,SigmaA)
+		Y <- MASS::mvrnorm(N,muB,SigmaB)
+		D <- KLD(X,Y,de="ks")
+		E <- exact_normal_kld(muA,SigmaA,muB,SigmaB)
+		expect_lt(abs(D-E),1e-2*length(muA))
+	}
+})
+
+test_that("KLD for multivariate normal, with mvtnorm",{
+	muA <- c(0,0)
+	SigmaA <- matrix(c(1,0.2,0.2,1),2,2)
+	muB <- c(0.1,-0.2)
+	SigmaB <- matrix(c(1,0.4,0.4,1),2,2)
+	if (requireNamespace("MASS", quietly = TRUE)){
+		set.seed(42)
+		N <- 1e3
+		X <- MASS::mvrnorm(N,muA,SigmaA)
+		Y <- MASS::mvrnorm(N,muB,SigmaB)
+		D <- KLD(X,Y,de="mvtnorm")
 		E <- exact_normal_kld(muA,SigmaA,muB,SigmaB)
 		expect_lt(abs(D-E),1e-2*length(muA))
 	}
