510a03360013b97776bedb86a069daca08accac6
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2026-08-31 12:05:30 +0200

    small text changes

--- R/uqsa-package.R
+++ R/uqsa-package.R
@@ -8,7 +8,7 @@
 #' @importFrom grDevices contourLines hcl.colors rgb
 #' @importFrom graphics arrows axis hist image legend lines matplot pairs par polygon text rect
 #' @importFrom methods as is show
-#' @importFrom stats acf approx cor cov dnorm dunif median na.omit quantile rnorm runif sd spline var
+#' @importFrom stats acf approx cor cov dnorm dunif median na.omit quantile rnorm runif sd spline var IQR
 #' @importFrom utils flush.console head read.delim tail
 ## usethis namespace: end
 NULL
